899800a65d8ec58f61000b6213f7941f887aea7f
Herintsoacr <herintsoacr@gmail.com>
2025-08-29 11:06:54 +0300

    fix: mariadb compose

--- srcs/requirements/mariadb/tools/launch.sh
+++ srcs/requirements/mariadb/tools/launch.sh
@@ -1,5 +1,15 @@
 #!bin/bash
 
-mysqld;
+exec mysqld;
 
-ALTER 'root'@'localhost' IDENTIFIED BY ;
+DB_ROOT_PASSWORD = $(cat /run/secrets/db_root_password)
+DB_USER_PASSWORD = $(cat /run/secrets/db_user_password)
+
+ALTER 'root'@'localhost' IDENTIFIED BY $DB_ROOT_PASSWORD;
+
+CREATE DATABASE $DATABASE_NAME;
+
+CREATE USER $DATABASE_USERNAME@'localhost' IDENTIFIED BY $DB_USER_PASSWORD;
+
+GRANT ALL PRIVILEGES ON $DATABASE_NAME.* TO $DATABASE_USERNAME@'localhost';
+FLUSH PRIVILEGES;
