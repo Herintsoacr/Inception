9a2e105a1bd49cf54e272558f7fd24135ec4aeeb
Herintsoacr <herintsoacr@gmail.com>
2025-08-31 13:47:27 +0300

    fix: mariadb launch

--- srcs/requirements/mariadb/tools/launch.sh
+++ srcs/requirements/mariadb/tools/launch.sh
@@ -3,18 +3,17 @@
 DB_ROOT_PASSWORD = $(cat /run/secrets/db_root_password)
 DB_USER_PASSWORD = $(cat /run/secrets/db_user_password)
 
-exec mysql;
+exec mysqld;
 
-until mysqladmin ping &>/dev/null; do
+until mysqladmin ping -u root -p${DB_ROOT_PASSWORD} --silent; do
 	sleep 1
 done
 
-mysql -e "ALTER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASSWORD}'; \
-			CREATE DATABASE ${DATABASE_NAME}; \
-			CREATE USER $DATABASE_USERNAME@'localhost' IDENTIFIED BY ${DB_USER_PASSWORD}; \
+mysql -u root -p${DB_ROOT_PASSWORD} -e "CREATE DATABASE ${DATABASE_NAME}; \
+			CREATE USER ${DATABASE_USERNAME}@'localhost' IDENTIFIED BY ${DB_USER_PASSWORD}; \
 			GRANT ALL PRIVILEGES ON ${DATABASE_NAME}.* TO ${DATABASE_USERNAME}@'localhost'; \
-			FLUSH PRIVILEGES;";
+			FLUSH PRIVILEGES;"
 
-mysqladmin -u root -p shutdown;
+mysqladmin shutdown -u root -p${DB_ROOT_PASSWORD}
 
-exec mysql;
+exec mysqld;
