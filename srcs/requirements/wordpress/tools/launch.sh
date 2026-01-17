43aa40504b05ad5881d2886965e866acccae08b9
Herintsoacr <herintsoacr@gmail.com>
2026-01-17 19:41:27 +0300

    push

--- srcs/requirements/wordpress/tools/launch.sh
+++ srcs/requirements/wordpress/tools/launch.sh
@@ -1,28 +1,32 @@
 #!/bin/bash
 
+DB_PASSWORD=$(cat /run/secrets/db_user_password)
+WP_PASSWORDS=$(cat /run/secrets/wp_root_password)
+WP_USER_PASS=$(cat /run/secrets/user_wp_password)
+
 if [ ! -f wp-config.php ]; then
 	wp core download --allow-root
 
 	wp config create \
-    --dbname="$DB_NAME" \
-    --dbuser="$DB_USER" \
-    --dbpass="$DB_PASSWORD" \
-    --dbhost="$DB_HOST" \
+    --dbname=$DB_NAME \
+    --dbuser=$DB_USER \
+    --dbpass=$DB_PASSWORD \
+    --dbhost=$DB_HOST \
 	--allow-root \
     --skip-check \
 
 	wp core install \
-  --url="$DOMAIN_NAME" \
+  --url=$DOMAIN_NAME \
   --title="Inception" \
-  --admin_user=WP_USERNAME \
+  --admin_user=$WP_USERNAME \
   --admin_password=$WP_PASSWORDS \
-  --admin_email=WP_ADMIN_MAIL \
+  --admin_email=$WP_ADMIN_MAIL \
   --allow-root \
 
 	wp user create $WP_USER $WP_EMAIL \
   --role=editor \
   --allow-root \
-  --user_pass=WP_USER_PASS
+  --user_pass=$WP_USER_PASS
 fi
 
 
